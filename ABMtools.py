559f6d4941291188fd823475036657af8389d207
dimaba <diekobakker@hotmail.com>
2015-12-06 20:54:25 +0100

    Make registering with controller and group optional in hatch()

--- ABMtools.py
+++ ABMtools.py
@@ -36,14 +36,23 @@ class Agent:
     def __str__(self):
         return "Type = Agent, Identity = {}, Group = {}".format(self.ident, self.group)
 
-    def hatch(self):
-        """ Create one new agent in the same group which copies all attributes other than ident."""
+    def hatch(self, register_with_group=True, register_with_controller=True):
+        """
+        Create one new agent in the same group which copies all attributes other than ident.
+        By default register this new agent with original agent's group and controller
+        :param register_with_group: Boolean, if True register new agent with original agent's group
+        :param register_with_controller: Boolean, if True register new agent with original agent's controller
+        :return: Return the new agent (in case you immediately want to make some modifications to new agent
+        """
         new_agent = copy.copy(self)
         new_agent.ident = new_agent.get_ident()
-        if self.group is not None:
+        if register_with_group and self.group is not None:
             group = self.controller.group(self.group)
             group.members.append(new_agent)
             group.update_size()
+        if register_with_controller and self.controller is not None:
+            self.controller.agents.append(new_agent)
+            self.controller.update_counts()
         return new_agent
         
         
