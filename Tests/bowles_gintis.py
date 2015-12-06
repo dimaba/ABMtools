1526f97989f4a2303eb62422cc5d422064b55989
dimaba <diekobakker@hotmail.com>
2015-12-06 23:38:19 +0100

    Minor typo

--- Tests/bowles_gintis.py
+++ Tests/bowles_gintis.py
@@ -1,6 +1,6 @@
 import os
 parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-os.sys.path.insert(0,parentdir)
+os.sys.path.insert(0, parentdir)
 
 import ABMtools
 import random
